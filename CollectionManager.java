5994f5a2bb2277a108681365c3a616074bd91707
cute_kitty29 <sonamarkova849@gmail.com>
2026-03-24 23:30:32 +0300

    Some changes in the Collection Manager

--- CollectionManager.java
+++ CollectionManager.java
@@ -1,9 +1,32 @@
+import java.io.BufferedInputStream;
+import  java.io.BufferedWriter;
+import  java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.ArrayList;
-
 
 public class CollectionManager{
     ArrayList<Object> objectList = new ArrayList<>();
     public void add (Object ob){
         objectList.add(ob);
+    }
+
+    public void load(String fileName) {
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileName))) {
+            // прочитать XML
+            // распарсить (можно через DOM / вручную)
+        } 
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Ошибка загрузки файла");
+        }   
+    }
+
+    public void save(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            
+        }
+        catch (Exception e) {
+            System.out.println("Ошибка сохранения");
+        }
     }
 }
