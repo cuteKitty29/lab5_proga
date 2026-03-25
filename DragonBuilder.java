7f878c9c02b351a91349e48620274e326fe3b5c9
cute_kitty29 <sonamarkova849@gmail.com>
2026-03-25 21:19:32 +0300

    created class DragonBuilder

---
+++ DragonBuilder.java
@@ -0,0 +1,52 @@
+public class implements DragonBuilder{
+
+    public DragonBuilder(){
+        Dragon thisDragon = new Dragon();
+    }
+
+    public DragonBuilder setName(String name){
+        thisDragon.setName(name);
+        return this;
+    }
+
+    public DragonBuilder setCoordinates(Coordinates coordinates){
+        thisDragon.setCoordinates(coordinates);
+        return this;
+    }
+    
+    public DragonBuilder setCreationDate(Date date){
+        thisDragon.setCreationDate(date);
+        return this;
+    }
+
+    public DragonBuilder setAge(long age){
+        thisDragon.setAge(age);
+        return this;
+    }
+
+    public DragonBuilder setColor(Color color){
+        thisDragon.setColor(color);
+        return this;
+    }
+
+    public DragonBuilder setType(DragonType type){
+        thisDragon.setType(type);
+        return this;
+    }
+
+    public DragonBuilder setHead(DragonHead head){
+        thisDragon.setHead(head);
+        return this;
+    }
+
+    public DragonBuilder setCharacter(DragonCharacter character){
+        thisDragon.setCharacter(character);
+        return this;
+    }
+    
+    
+    
+    public Dragon create(){
+        return thisDragon;
+    }
+}
